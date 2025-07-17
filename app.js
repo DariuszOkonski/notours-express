3ed9a4e1ad232353045b9cfc5e2f3a2e6403fbb7
Dariusz Okoński <darek200180@gmail.com>
2025-07-17 08:30:31 +0200

    initialization

---
+++ app.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+const PATH = `${__dirname}/dev-data/data/tours-simple.json`;
+const tours = JSON.parse(fs.readFileSync(PATH));
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(PATH, JSON.stringify(tours), (err) => {
+    return res.status(201).json({ status: 'success', data: { tour: newTour } });
+  });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+
+  if (id > tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+
+  const tour = tours.find((el) => el.id === id);
+
+  return res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
