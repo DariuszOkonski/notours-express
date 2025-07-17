6350d7f30cacccb28741d6572901e8476a53aaaa
Dariusz Okoński <darek200180@gmail.com>
2025-07-17 08:43:31 +0200

    add patch endpoint

--- app.js
+++ app.js
@@ -18,16 +18,6 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
-app.post('/api/v1/tours', (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-  fs.writeFile(PATH, JSON.stringify(tours), (err) => {
-    return res.status(201).json({ status: 'success', data: { tour: newTour } });
-  });
-});
-
 app.get('/api/v1/tours/:id', (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
@@ -40,6 +30,33 @@ app.get('/api/v1/tours/:id', (req, res) => {
     status: 'success',
     data: {
       tour,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(PATH, JSON.stringify(tours), (err) => {
+    return res.status(201).json({ status: 'success', data: { tour: newTour } });
+  });
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  return res.status(200).json({
+    status: 'success',
+    data: {
+      tour: 'Updated tour here',
     },
   });
 });
