f3587c9920019423250f3fab5315306a02e50a33
Dariusz Okoński <darek200180@gmail.com>
2025-07-17 08:33:31 +0200

    check if tour is available

--- app.js
+++ app.js
@@ -30,12 +30,11 @@ app.post('/api/v1/tours', (req, res) => {
 
 app.get('/api/v1/tours/:id', (req, res) => {
   const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
 
-  if (id > tours.length) {
+  if (!tour) {
     return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
   }
-
-  const tour = tours.find((el) => el.id === id);
 
   return res.status(200).json({
     status: 'success',
