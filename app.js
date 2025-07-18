ec96a6282862d3f4d701a2d0fb71130b099e24a1
Dariusz Okoński <darek200180@gmail.com>
2025-07-18 07:54:37 +0200

    remove unused comments

--- app.js
+++ app.js
@@ -65,12 +65,6 @@ const deleteTour = (req, res) => {
   return res.status(204).json({ status: 'success', data: null });
 };
 
-// app.get('/api/v1/tours', getAllTours);
-// app.get('/api/v1/tours/:id', getTour);
-// app.post('/api/v1/tours', createTour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
-
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
 app
   .route('/api/v1/tours/:id')
