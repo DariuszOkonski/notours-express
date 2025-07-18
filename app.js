a805ebef28d65065f73cab6a16324f429a407b1a
Dariusz Okoński <darek200180@gmail.com>
2025-07-18 08:31:17 +0200

    add morgan lib

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
+app.use(morgan('dev'));
 app.use(express.json());
 
-app.use((req, res, next) => {
-  console.log('Hello from the middleware!!!');
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('Hello from the middleware!!!');
+//   next();
+// });
 
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
+// app.use((req, res, next) => {
+//   req.requestTime = new Date().toISOString();
+//   next();
+// });
 
 const PATH = `${__dirname}/dev-data/data/tours-simple.json`;
 const tours = JSON.parse(fs.readFileSync(PATH));
 
 const getAllTours = (req, res) => {
-  console.log(req.requestTime);
+  // console.log(req.requestTime);
+
   res.status(200).json({
     status: 'success',
-    requestedAt: req.requestTime,
+    // requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours,
