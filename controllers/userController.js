344af3cfb88fa04eb7aca5ae9ead4a6ff4c8fceb
Dariusz Okoński <darek200180@gmail.com>
2025-07-22 08:35:22 +0200

    add userController

---
+++ controllers/userController.js
@@ -0,0 +1,19 @@
+exports.getAllUsers = (req, res) => {
+  return res.status(500).json({ status: 'error', message: 'getAllUsers' });
+};
+
+exports.createUser = (req, res) => {
+  return res.status(500).json({ status: 'error', message: 'createUser' });
+};
+
+exports.getUser = (req, res) => {
+  return res.status(500).json({ status: 'error', message: 'getUser' });
+};
+
+exports.updateUser = (req, res) => {
+  return res.status(500).json({ status: 'error', message: 'updateUser' });
+};
+
+exports.deleteUser = (req, res) => {
+  return res.status(500).json({ status: 'error', message: 'deleteUser' });
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,28 +1,16 @@
 const express = require('express');
+const userController = require('../controllers/userController');
 
 const router = express.Router();
 
-const getAllUsers = (req, res) => {
-  return res.status(500).json({ status: 'error', message: 'getAllUsers' });
-};
-
-const createUser = (req, res) => {
-  return res.status(500).json({ status: 'error', message: 'createUser' });
-};
-
-const getUser = (req, res) => {
-  return res.status(500).json({ status: 'error', message: 'getUser' });
-};
-
-const updateUser = (req, res) => {
-  return res.status(500).json({ status: 'error', message: 'updateUser' });
-};
-
-const deleteUser = (req, res) => {
-  return res.status(500).json({ status: 'error', message: 'deleteUser' });
-};
-
-router.route('/').get(getAllUsers).post(createUser);
-router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
+router
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 module.exports = router;
