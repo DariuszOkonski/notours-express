3c0b8951ac0320ca2b7d5903fc99f73e57869fe9
Dariusz Okoński <darek200180@gmail.com>
2025-07-23 08:07:34 +0200

    add middleware checkBody

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,11 +3,22 @@ const fs = require('fs');
 const PATH = `${__dirname}/../dev-data/data/tours-simple.json`;
 const tours = JSON.parse(fs.readFileSync(PATH));
 
-exports.checkId = (req, res, next, val) => {
+exports.checkID = (req, res, next, val) => {
   console.log('val: ', val);
 
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+
+  next();
+};
+
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price',
+    });
   }
 
   next();
