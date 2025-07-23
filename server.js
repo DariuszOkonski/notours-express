01878aaeb1cf457ab692acad60938d9e57569609
Dariusz Okoński <darek200180@gmail.com>
2025-07-23 08:30:38 +0200

    add env variables

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+const dotenv = require('dotenv');
 const app = require('./app');
 
+dotenv.config({ path: './config.env' });
+
 // listener
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
