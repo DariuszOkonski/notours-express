554b8399cf8c7a9e708ccc58441b3b6f633b20fd
Dariusz Okoński <darek200180@gmail.com>
2025-07-29 07:52:33 +0200

    connect to mongoDB

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+// Connect to MongoDB
+const DB = process.env.DATABASE_LOCAL;
+
+mongoose
+  .connect(DB)
+  .then(() => console.log('DB connection successful!'))
+  .catch((err) => console.log('DB connection error:', err));
 
 // listener
 const port = process.env.PORT || 3000;
