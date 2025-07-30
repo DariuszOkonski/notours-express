ec5625fb2b38d11beb49769c13e243712fe6340c
Dariusz Okoński <darek200180@gmail.com>
2025-07-30 07:02:35 +0200

    connect to localDB

--- server.js
+++ server.js
@@ -9,8 +9,12 @@ const app = require('./app');
 const DB = process.env.DATABASE_LOCAL;
 
 mongoose
-  .connect(DB)
-  .then(() => console.log('DB connection successful!'))
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful...'))
   .catch((err) => console.log('DB connection error:', err));
 
 // listener
