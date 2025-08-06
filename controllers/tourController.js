c665488462aaf2d3abedf5a750b6734e8ac9d183
Dariusz Okoński <darek200180@gmail.com>
2025-08-06 08:20:16 +0200

    add sorting by price and ratingsAverage

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,17 +3,26 @@ const Tour = require('./../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
     // build query
-    // 1 filtering
+    // 1A filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
 
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    // 2 advanced filtering
+    // 1B advanced filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2 sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
 
     // execute query
     const tours = await query;
